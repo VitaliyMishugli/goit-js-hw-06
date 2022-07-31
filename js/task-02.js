c5f2c838708a0aaef2f043ef6751ecde5f1e25ad
Vitaliy Mishugli <vmishughli@gmail.com>
2022-07-31 20:29:33 +0100

    task_2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,4 @@
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +7,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+/**
+ * 
+ * @param {string} root 
+ * @param {Array} ingredientsArray 
+ */
+
+function createIngredientsList(root, ingredientsArray) {
+  const rootNode = document.querySelector(root);
+  console.log(rootNode);
+  const finalListArray = [];
+  for (let i = 0; i < ingredientsArray.length; i += 1) {
+    const elem = document.createElement('li');
+    elem.textContent = ingredientsArray[i];
+    elem.classList.add('item');
+
+    finalListArray.push(elem);
+  }
+  rootNode.append(...finalListArray);
+}
+
+createIngredientsList("#ingredients", ingredients);
