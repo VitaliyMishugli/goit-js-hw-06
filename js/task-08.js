8a4f4c0f45fefc12176b3aff73b797d0306ca9b4
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-01 15:12:37 +0100

    task_9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  const arrayElements = [...formElements];
+  const formData = {};
+
+  for (let i = 0; i < arrayElements.length; i += 1){    
+    if (arrayElements[i].type !== 'submit') {
+      if (arrayElements[i].value !== '') {
+        const mail = formElements.email.value;
+        const password = formElements.password.value;
+        formData.mail = mail;
+        formData.password = password;
+      }
+      else {
+        alert("Всі поля повинні бути заповнені");
+      }
+    }
+  }
+  console.log(formData);
+   event.target.reset(); 
+})
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const output = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  let color = getRandomHexColor();
+  output.textContent = color;
+  body.style.backgroundColor = color;
+})
