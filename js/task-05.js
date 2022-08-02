caae708f9ee4e1f0a10b551df62aa288869677e3
Vitaliy Mishugli <vmishughli@gmail.com>
2022-08-02 20:43:52 +0100

    Deletes console.log

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener('input', () => {
-  console.log(input.value);
   if (input.value === '') {
     output.textContent = "Anonymous";
   }
